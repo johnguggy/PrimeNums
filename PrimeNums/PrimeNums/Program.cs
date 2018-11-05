bebc75b5d00a127ef1c2202048d92e013d923d39
John Guggenbiller <42472160+johnguggy@users.noreply.github.com>
2018-11-05 15:58:47 -0500

    Add files via upload

---
+++ PrimeNums/PrimeNums/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int counter = 1; counter <=100; counter++)
+            {
+                bool flag = true;
+                for (int prime = 2; prime <= counter - 1; prime++)
+                {
+                    if (counter % prime == 0)
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag == true)
+                {
+                    Console.WriteLine(counter);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
